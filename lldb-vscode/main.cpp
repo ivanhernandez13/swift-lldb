4549976c8dd28780a44bf31bea97fb42f7c11807
Ivan Hernandez <ivanhernandez@ivanhernandez-imacpro.roam.corp.google.com>
2019-04-27 17:03:11 -0400

    Setup swift 5.0 branch with lldb-vscode

---
+++ lldb-vscode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lldb-vscode
+//
+//  Created by Ivan Hernandez on 4/27/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
